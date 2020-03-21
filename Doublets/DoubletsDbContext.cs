c0adf8745f20e842478f0744fc9eda73f319d961
Konstantin Diachenko <drakonard@gmail.com>
2020-03-21 17:47:49 +0300

    External references enabled.

--- Doublets/DoubletsDbContext.cs
+++ Doublets/DoubletsDbContext.cs
@@ -15,7 +15,7 @@ using Platform.Data.Doublets.Sequences;
 using Platform.Data.Doublets.Sequences.Walkers;
 using Platform.Data.Doublets.Sequences.Converters;
 using Comparisons.SQLiteVSDoublets.Model;
-using LinkAddress = System.UInt64;
+using TLinkAddress = System.UInt64;
 using Platform.Data.Doublets.CriterionMatchers;
 using Platform.Data.Doublets.Memory.Split.Specific;
 using System.IO;
@@ -25,32 +25,34 @@ namespace Comparisons.SQLiteVSDoublets.Doublets
 {
     public class DoubletsDbContext : DisposableBase
     {
-        private readonly LinkAddress _meaningRoot;
-        private readonly LinkAddress _unicodeSymbolMarker;
-        private readonly LinkAddress _unicodeSequenceMarker;
-        private readonly LinkAddress _titlePropertyMarker;
-        private readonly LinkAddress _contentPropertyMarker;
-        private readonly LinkAddress _publicationDateTimePropertyMarker;
-        private readonly LinkAddress _blogPostMarker;
-        private readonly PropertiesOperator<LinkAddress> _defaultLinkPropertyOperator;
-        private readonly RawNumberToAddressConverter<LinkAddress> _numberToAddressConverter;
-        private readonly AddressToRawNumberConverter<LinkAddress> _addressToNumberConverter;
-        private readonly IConverter<string, LinkAddress> _stringToUnicodeSequenceConverter;
-        private readonly IConverter<LinkAddress, string> _unicodeSequenceToStringConverter;
-        private readonly ILinks<LinkAddress> _disposableLinks;
-        private readonly ILinks<LinkAddress> _links;
+        private readonly TLinkAddress _meaningRoot;
+        private readonly TLinkAddress _unicodeSymbolMarker;
+        private readonly TLinkAddress _unicodeSequenceMarker;
+        private readonly TLinkAddress _titlePropertyMarker;
+        private readonly TLinkAddress _contentPropertyMarker;
+        private readonly TLinkAddress _publicationDateTimePropertyMarker;
+        private readonly TLinkAddress _blogPostMarker;
+        private readonly PropertiesOperator<TLinkAddress> _defaultLinkPropertyOperator;
+        private readonly RawNumberToAddressConverter<TLinkAddress> _numberToAddressConverter;
+        private readonly AddressToRawNumberConverter<TLinkAddress> _addressToNumberConverter;
+        private readonly IConverter<string, TLinkAddress> _stringToUnicodeSequenceConverter;
+        private readonly IConverter<TLinkAddress, string> _unicodeSequenceToStringConverter;
+        private readonly ILinks<TLinkAddress> _disposableLinks;
+        private readonly ILinks<TLinkAddress> _links;
 
         public DoubletsDbContext(string dbFilename)
         {
             var dataMemory = new FileMappedResizableDirectMemory(dbFilename);
             var indexMemory = new FileMappedResizableDirectMemory($"{Path.GetFileNameWithoutExtension(dbFilename)}.links.index");
 
+            var linksConstants = new LinksConstants<TLinkAddress>(enableExternalReferencesSupport: true);
+
             // Init the links storage
-            _disposableLinks = new UInt64SplitMemoryLinks(dataMemory, indexMemory); // Low-level logic
+            _disposableLinks = new UInt64SplitMemoryLinks(dataMemory, indexMemory, UInt64SplitMemoryLinks.DefaultLinksSizeStep, linksConstants); // Low-level logic
             _links = new UInt64Links(_disposableLinks); // Main logic in the combined decorator
 
             // Set up constant links (markers, aka mapped links)
-            LinkAddress currentMappingLinkIndex = 1;
+            TLinkAddress currentMappingLinkIndex = 1;
             _meaningRoot = GerOrCreateMeaningRoot(currentMappingLinkIndex++);
             _unicodeSymbolMarker = GetOrCreateNextMapping(currentMappingLinkIndex++);
             _unicodeSequenceMarker = GetOrCreateNextMapping(currentMappingLinkIndex++);
@@ -60,48 +62,48 @@ namespace Comparisons.SQLiteVSDoublets.Doublets
             _blogPostMarker = GetOrCreateNextMapping(currentMappingLinkIndex++);
 
             // Create properties operator that is able to control reading and writing properties for any link (object)
-            _defaultLinkPropertyOperator = new PropertiesOperator<LinkAddress>(_links);
+            _defaultLinkPropertyOperator = new PropertiesOperator<TLinkAddress>(_links);
 
             // Create converters that are able to convert link's address (UInt64 value) to a raw number represented with another UInt64 value and back
-            _numberToAddressConverter = new RawNumberToAddressConverter<LinkAddress>();
-            _addressToNumberConverter = new AddressToRawNumberConverter<LinkAddress>();
+            _numberToAddressConverter = new RawNumberToAddressConverter<TLinkAddress>();
+            _addressToNumberConverter = new AddressToRawNumberConverter<TLinkAddress>();
 
             // Create converters that are able to convert string to unicode sequence stored as link and back
-            var balancedVariantConverter = new BalancedVariantConverter<LinkAddress>(_links);
-            var unicodeSymbolCriterionMatcher = new TargetMatcher<LinkAddress>(_links, _unicodeSymbolMarker);
-            var unicodeSequenceCriterionMatcher = new TargetMatcher<LinkAddress>(_links, _unicodeSequenceMarker);
-            var charToUnicodeSymbolConverter = new CharToUnicodeSymbolConverter<LinkAddress>(_links, _addressToNumberConverter, _unicodeSymbolMarker);
-            var unicodeSymbolToCharConverter = new UnicodeSymbolToCharConverter<LinkAddress>(_links, _numberToAddressConverter, unicodeSymbolCriterionMatcher);
-            var sequenceWalker = new RightSequenceWalker<LinkAddress>(_links, new DefaultStack<LinkAddress>(), unicodeSymbolCriterionMatcher.IsMatched);
-            _stringToUnicodeSequenceConverter = new CachingConverterDecorator<string, LinkAddress>(new StringToUnicodeSequenceConverter<LinkAddress>(_links, charToUnicodeSymbolConverter, balancedVariantConverter, _unicodeSequenceMarker));
-            _unicodeSequenceToStringConverter = new CachingConverterDecorator<LinkAddress, string>(new UnicodeSequenceToStringConverter<LinkAddress>(_links, unicodeSequenceCriterionMatcher, sequenceWalker, unicodeSymbolToCharConverter));
+            var balancedVariantConverter = new BalancedVariantConverter<TLinkAddress>(_links);
+            var unicodeSymbolCriterionMatcher = new TargetMatcher<TLinkAddress>(_links, _unicodeSymbolMarker);
+            var unicodeSequenceCriterionMatcher = new TargetMatcher<TLinkAddress>(_links, _unicodeSequenceMarker);
+            var charToUnicodeSymbolConverter = new CharToUnicodeSymbolConverter<TLinkAddress>(_links, _addressToNumberConverter, _unicodeSymbolMarker);
+            var unicodeSymbolToCharConverter = new UnicodeSymbolToCharConverter<TLinkAddress>(_links, _numberToAddressConverter, unicodeSymbolCriterionMatcher);
+            var sequenceWalker = new RightSequenceWalker<TLinkAddress>(_links, new DefaultStack<TLinkAddress>(), unicodeSymbolCriterionMatcher.IsMatched);
+            _stringToUnicodeSequenceConverter = new CachingConverterDecorator<string, TLinkAddress>(new StringToUnicodeSequenceConverter<TLinkAddress>(_links, charToUnicodeSymbolConverter, balancedVariantConverter, _unicodeSequenceMarker));
+            _unicodeSequenceToStringConverter = new CachingConverterDecorator<TLinkAddress, string>(new UnicodeSequenceToStringConverter<TLinkAddress>(_links, unicodeSequenceCriterionMatcher, sequenceWalker, unicodeSymbolToCharConverter));
         }
 
-        private LinkAddress GerOrCreateMeaningRoot(LinkAddress meaningRootIndex) => _links.Exists(meaningRootIndex) ? meaningRootIndex : _links.CreatePoint();
+        private TLinkAddress GerOrCreateMeaningRoot(TLinkAddress meaningRootIndex) => _links.Exists(meaningRootIndex) ? meaningRootIndex : _links.CreatePoint();
 
-        private LinkAddress GetOrCreateNextMapping(LinkAddress currentMappingIndex) => _links.Exists(currentMappingIndex) ? currentMappingIndex : _links.CreateAndUpdate(_meaningRoot, _links.Constants.Itself);
+        private TLinkAddress GetOrCreateNextMapping(TLinkAddress currentMappingIndex) => _links.Exists(currentMappingIndex) ? currentMappingIndex : _links.CreateAndUpdate(_meaningRoot, _links.Constants.Itself);
 
-        public string ConvertToString(LinkAddress sequence) => _unicodeSequenceToStringConverter.Convert(sequence);
+        public string ConvertToString(TLinkAddress sequence) => _unicodeSequenceToStringConverter.Convert(sequence);
 
-        public LinkAddress ConvertToSequence(string @string) => _stringToUnicodeSequenceConverter.Convert(@string);
+        public TLinkAddress ConvertToSequence(string @string) => _stringToUnicodeSequenceConverter.Convert(@string);
 
         public IList<BlogPost> BlogPosts => GetBlogPosts();
 
         public IList<BlogPost> GetBlogPosts()
         {
-            var list = new List<IList<LinkAddress>>();
-            var listFiller = new ListFiller<IList<LinkAddress>, LinkAddress>(list, _links.Constants.Continue);
+            var list = new List<IList<TLinkAddress>>();
+            var listFiller = new ListFiller<IList<TLinkAddress>, TLinkAddress>(list, _links.Constants.Continue);
             // Load all links that match the query: (any: _blogPostMarker any) it means, link with any address, _blogPostMarker as source and any link as target.
             // All links that match this query are BlogPosts.
             var any = _links.Constants.Any;
-            var query = new Link<LinkAddress>(any, _blogPostMarker, any);
+            var query = new Link<TLinkAddress>(any, _blogPostMarker, any);
             _links.Each(listFiller.AddAndReturnConstant, query);
             return list.Select(LoadBlogPost).ToList();
         }
 
-        public BlogPost LoadBlogPost(IList<LinkAddress> postLink) => LoadBlogPost(postLink[_links.Constants.IndexPart]);
+        public BlogPost LoadBlogPost(IList<TLinkAddress> postLink) => LoadBlogPost(postLink[_links.Constants.IndexPart]);
 
-        public BlogPost LoadBlogPost(LinkAddress postLink)
+        public BlogPost LoadBlogPost(TLinkAddress postLink)
         {
             var blogPost = new BlogPost();
 
@@ -121,7 +123,7 @@ namespace Comparisons.SQLiteVSDoublets.Doublets
             return blogPost;
         }
 
-        public LinkAddress SaveBlogPost(BlogPost post)
+        public TLinkAddress SaveBlogPost(BlogPost post)
         {
             var newPostLink = _links.CreateAndUpdate(_blogPostMarker, _links.Constants.Itself);
 
@@ -137,7 +139,7 @@ namespace Comparisons.SQLiteVSDoublets.Doublets
             return newPostLink;
         }
 
-        public void Delete(LinkAddress link) => _links.Delete(link);
+        public void Delete(TLinkAddress link) => _links.Delete(link);
 
         protected override void Dispose(bool manual, bool wasDisposed)
         {
